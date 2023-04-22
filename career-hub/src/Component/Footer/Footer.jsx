f38e709e6a33aec0d5f4b6422a68730e827a8e97
utshobarua10 <utshobarua10@gmail.com>
2023-04-22 20:06:45 +0600

    added the footer

--- career-hub/src/Component/Footer/Footer.jsx
+++ career-hub/src/Component/Footer/Footer.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import facebook from '../../All Images/images/facebook.png'
+import instagram from '../../All Images/images/instagram.png'
+import twitter from '../../All Images/images/twitter.png'
+
 
 const Footer = () => {
     return (
-        <div>
-            <h3>Halo vai this is the footer</h3>
+        <div className='flex justify-center space-x-10 bg-black p-10 text-white'>
+            <div className='1st-column'>
+                <p className='text-2xl font-extrabold'>Job Hub</p>
+                <p>There are many variations of passages of Lorem Ipsum , <br /> but the majority have suffered alteration in some form.</p>
+                <div className='flex space-x-5 mt-5'>
+                    <img src={facebook} className='w-[50px]' alt="" />
+                    <img src={instagram} className='w-[50px]' alt="" />
+                    <img src={twitter} className='w-[50px]' alt="" />
+                </div>
+            </div>
+            <div className='2nd-column'>
+                <p className='text-2xl font-extrabold'>Company</p>
+                <p>About Us</p>
+                <p>Work</p>
+                <p>Latest News</p>
+                <p>Carrers</p>
+            </div>
+            <div className='3rd-column '>
+                <p className='text-2xl font-extrabold'>Product</p>
+                <p>Prototype</p>
+                <p>Plans & Pricing</p>
+                <p>Customers</p>
+                <p>Integration</p>
+            </div>
+            <div className='4th-column'>
+                <p className='text-2xl font-extrabold'>Support</p>
+                <p>Help Desk</p>
+                <p>Sales</p>
+                <p>Become a partner</p>
+                <p>Developers</p>
+            </div>
+            <div className='5th-column'>
+                <p >524 Broadway , NYC</p>
+                <p>+1 777 - 978 - 5570</p>
+            </div>
+
+
         </div>
     );
 };
